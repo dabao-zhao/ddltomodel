69a976cdcf342bd5a73484ca4190796e3d749641
dabao-zhao <zhao.jiaxing@qq.com>
2022-11-16 10:54:55 +0800

    SetTableName

--- template/tablename.go
+++ template/tablename.go
@@ -5,4 +5,8 @@ const TableNameTpl = `
 func (m *default{{.upperStartCamelObject}}Model) TableName() string {
 	return m.table
 }
+
+func (m *default{{.upperStartCamelObject}}Model) SetTableName(tableName string) {
+	m.table = tableName
+}
 `
